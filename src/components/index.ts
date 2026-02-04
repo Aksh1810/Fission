4145b7a75a85362a91dfebfe79c1117e87ae1730
Aksh1810 <akshp427@gmail.com>
2026-02-04 17:46:18 -0600

    add component barrel exports

---
+++ src/components/index.ts
@@ -0,0 +1,11 @@
+/**
+ * Component exports
+ */
+
+export { CellDots } from './CellDots';
+export { BurstEffect } from './BurstEffect';
+export { GameCell } from './GameCell';
+export { GameBoard } from './GameBoard';
+export { ColorBar } from './ColorBar';
+export { Modal, GameOverModal, DifficultyModal } from './Modal';
+export { Navigation } from './Navigation';
